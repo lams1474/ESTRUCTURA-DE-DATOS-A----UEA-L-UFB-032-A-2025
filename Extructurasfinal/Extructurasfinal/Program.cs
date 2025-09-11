e0803851f091e4749ab70fa864797053cc544143
Lams1474 <la.maiguas@uea.edu.ec>
2025-09-10 22:02:30 -0500

    commit3

--- Extructurasfinal/Extructurasfinal/Program.cs
+++ Extructurasfinal/Extructurasfinal/Program.cs
@@ -3,23 +3,36 @@ Console.WriteLine("===== Universidad Estatal Amazonica =====");
 Console.WriteLine("===== Tercer Semestre =====");
 Console.WriteLine("===== Luis Alfonso Maigua Sisalema =====");
 
-Diccionario.run();
-
-Vacunas.run();
-
-Torneofutbol.run();
-
-Catalogorevista.run();
-
+// Semana 3 
 Estudiante.run();
 
+// Swmana 4 Simulador 
 Simulador30asientos.run();
 
+// Semana 5 Ejercicios
 Ejercicio1.run();
 Ejercicio10.run();
 Ejercicio3.run();
 Ejercicio4.run();
 Ejercicio8.run();
+
+// semana 10 Vacunas
+Vacunas.run();
+
+// Semana 11 Diccionario
+Diccionario.run();
+
+// semana 12 Torneo de Futbol
+Torneofutbol.run();
+
+// Semana 13 Catalogo Revistas
+Catalogorevista.run();
+
+
+
+
+
+
 
 
 
--- Extructurasfinal/Extructurasfinal/obj/Debug/net9.0/Extructurasfinal.AssemblyInfo.cs
+++ Extructurasfinal/Extructurasfinal/obj/Debug/net9.0/Extructurasfinal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Extructurasfinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c8c787e0b7a422b2d3f3e7fee134688dd3191a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3e9ca39611b1806352eb8c5cf5d0e771902b6f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Extructurasfinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Extructurasfinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
